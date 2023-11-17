3a34c5916fb4b229135fa8bf1647cf28e61d18f1
Harmedino <adamsahmed191@gmail.com>
2023-11-17 10:13:31 +0100

    navigation design

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavigationbarComponent } from '../navigationbar/navigationbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavigationbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
