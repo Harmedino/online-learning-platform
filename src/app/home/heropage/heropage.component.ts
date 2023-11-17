8747a48af9406545ed72d34e0b13fe9cc669f0f9
Harmedino <adamsahmed191@gmail.com>
2023-11-17 10:38:27 +0100

    started the hero page

---
+++ src/app/home/heropage/heropage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-heropage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './heropage.component.html',
+  styleUrl: './heropage.component.css'
+})
+export class HeropageComponent {
+
+}
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationbarComponent } from '../navigationbar/navigationbar.component';
+import { HeropageComponent } from '../heropage/heropage.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavigationbarComponent],
+  imports: [CommonModule, NavigationbarComponent,HeropageComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
