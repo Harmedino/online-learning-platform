551e3210b89fbd834d6c7cb13849847a6e4b0481
Harmedino <adamsahmed191@gmail.com>
2023-11-17 10:13:12 +0100

    header started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HomeComponent } from './home/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatSlideToggleModule],
+  imports: [CommonModule, RouterOutlet, MatSlideToggleModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
