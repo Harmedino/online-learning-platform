111e2cc3e6691820a15265a6a677c97b7475cd13
Damilarr <eaziaed@gmail.com>
2023-11-18 01:28:43 +0100

    added fonts,fontawesome,finished benefits,added some colors to config

--- src/app/home/heropage/heropage.component.ts
+++ src/app/home/heropage/heropage.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BenefitBoxComponent } from '../benefit-box/benefit-box.component';
 
 @Component({
   selector: 'app-heropage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BenefitBoxComponent],
   templateUrl: './heropage.component.html',
-  styleUrl: './heropage.component.css'
+  styleUrl: './heropage.component.css',
 })
-export class HeropageComponent {
-
-}
+export class HeropageComponent {}
