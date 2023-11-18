fd70d3fdc55978d2da26b3ddf2e382461976765e
Harmedino <adamsahmed191@gmail.com>
2023-11-18 09:38:03 +0100

    responsiveness fixed

--- src/app/home/navigationbar/navigationbar.component.ts
+++ src/app/home/navigationbar/navigationbar.component.ts
@@ -10,4 +10,9 @@ import { CommonModule } from '@angular/common';
 })
 export class NavigationbarComponent {
 
+  mobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
 }
