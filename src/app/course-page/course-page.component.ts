0c55b1c07014cd6e375b03921336b9761445da2c
Harmedino <adamsahmed191@gmail.com>
2024-01-07 12:17:26 +0100

    image added

--- src/app/course-page/course-page.component.ts
+++ src/app/course-page/course-page.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationbarComponent } from '../home/navigationbar/navigationbar.component';
 import { FooterComponent } from '../home/footer/footer.component';
-import { CourseHeroComponent } from '../coursePage/course-hero/course-hero.component';
 import { CurriculumComponent } from '../coursePage/curriculum/curriculum.component';
 
 @Component({
@@ -11,7 +10,6 @@ import { CurriculumComponent } from '../coursePage/curriculum/curriculum.compone
   imports: [CommonModule,
     NavigationbarComponent,
     FooterComponent,
-    CourseHeroComponent,
   CurriculumComponent],
   templateUrl: './course-page.component.html',
   styleUrl: './course-page.component.css'
