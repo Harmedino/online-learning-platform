541657abf1e17b09705b247874c8e7983cc541d2
Harmedino <adamsahmed191@gmail.com>
2024-01-08 14:54:13 +0100

    pricing route updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 import { CoursePageComponent } from './course-page/course-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { PricingpageComponent } from './pricingpage/pricingpage.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: "courses", component: CoursePageComponent },
-  {path:"about-us", component: AboutUsComponent}
+  { path: "about-us", component: AboutUsComponent },
+  {path:'pricing', component: PricingpageComponent}
 ];
--- src/app/pricingpage/pricingpage.component.ts
+++ src/app/pricingpage/pricingpage.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../home/footer/footer.component';
+import { NavigationbarComponent } from '../home/navigationbar/navigationbar.component';
 
 @Component({
   selector: 'app-pricingpage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    FooterComponent,
+  NavigationbarComponent],
   templateUrl: './pricingpage.component.html',
   styleUrl: './pricingpage.component.css'
 })
