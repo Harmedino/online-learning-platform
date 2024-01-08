d721b728bc53843dfa8a8318122df8d07ba9f336
Harmedino <adamsahmed191@gmail.com>
2024-01-08 14:43:04 +0100

    pricing page started

---
+++ src/app/pricingpage/pricingpage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pricingpage',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pricingpage.component.html',
+  styleUrl: './pricingpage.component.css'
+})
+export class PricingpageComponent {
+
+}
