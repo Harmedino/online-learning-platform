6b58a8bf004039821c8f2d650a969088e0f8fbaf
Harmedino <adamsahmed191@gmail.com>
2024-01-10 12:41:25 +0100

    all done now

--- src/app/home/pricing/pricing.component.ts
+++ src/app/home/pricing/pricing.component.ts
@@ -7,50 +7,67 @@ import { ActivatedRoute } from '@angular/router';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './pricing.component.html',
-  styleUrl: './pricing.component.css'
+  styleUrl: './pricing.component.css',
 })
 export class PricingComponent implements OnInit {
-
   currentRoute: string | undefined;
-  expression: number | boolean = false;
+  expression: number | boolean = true;
 
-  faq: { question: string, answer: string }[] = [
-    {question: 'Can I enroll in multiple courses at once?',
-    answer:'Absolutely! You can enroll in multiple courses simultaneously and access them at your convenience.'},
-    {question: 'Can I enroll in multiple courses at once?',
-    answer:'Absolutely! You can enroll in multiple courses simultaneously and access them at your convenience.'},
-    {question: 'Can I enroll in multiple courses at once?',
-    answer:'Absolutely! You can enroll in multiple courses simultaneously and access them at your convenience.'},
-    {question: 'Can I enroll in multiple courses at once?',
-    answer:'Absolutely! You can enroll in multiple courses simultaneously and access them at your convenience.'},
-    {question: 'Can I enroll in multiple courses at once?',
-    answer:'Absolutely! You can enroll in multiple courses simultaneously and access them at your convenience.'},
-  ]
-  constructor(private route:ActivatedRoute) {
+  faq: { question: string; answer: string }[] = [
+    {
+      question: 'Can I enroll in multiple courses at once?',
+      answer:
+        'Absolutely! You can enroll in multiple courses simultaneously and access them at your convenience.',
+    },
+    {
+      question: 'Can I enroll in multiple courses at once?',
+      answer:
+        'Absolutely! You can enroll in multiple courses simultaneously and access them at your convenience.',
+    },
+    {
+      question: 'Can I enroll in multiple courses at once?',
+      answer:
+        'Absolutely! You can enroll in multiple courses simultaneously and access them at your convenience.',
+    },
+    {
+      question: 'Can I enroll in multiple courses at once?',
+      answer:
+        'Absolutely! You can enroll in multiple courses simultaneously and access them at your convenience.',
+    },
+    {
+      question: 'Can I enroll in multiple courses at once?',
+      answer:
+        'Absolutely! You can enroll in multiple courses simultaneously and access them at your convenience.',
+    },
+  ];
 
-  }
+
+  svgChange: boolean[] = new Array(this.faq.length).fill(true);
+
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.url.subscribe(segments => {
+    this.route.url.subscribe((segments) => {
       // Extract the current route
       this.currentRoute = segments.join('/pricing');
       console.log(this.currentRoute);
-
     });
+
   }
 
   activeTab: 'monthly' | 'yearly' = 'monthly';
 
   setActiveTab(tab: 'monthly' | 'yearly'): string {
-    return this.activeTab = tab;
+    return (this.activeTab = tab);
   }
 
-  faqHandler(index:any) {
-
+  faqHandler(index: any) {
     if (this.expression === index) {
-    this.expression = false
+      this.expression = false;
     } else {
-      this.expression = index
+      this.expression = index;
     }
+    alert(this.svgChange)
+    this.svgChange[index] = !this.svgChange[index];
   }
 }
