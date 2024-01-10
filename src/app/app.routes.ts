514f00f10b8f1c395dc1f06912a64e45b50cef87
Harmedino <adamsahmed191@gmail.com>
2024-01-10 14:37:00 +0100

    done with sign up design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { CoursePageComponent } from './course-page/course-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { PricingpageComponent } from './pricingpage/pricingpage.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
+import { SignInComponent } from './authPage/sign-in/sign-in.component';
+import { SignUpComponent } from './authPage/sign-up/sign-up.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: "courses", component: CoursePageComponent },
   { path: "about-us", component: AboutUsComponent },
   { path: 'pricing', component: PricingpageComponent },
-  {path: 'contact-us', component:ContactPageComponent}
+  { path: 'contact-us', component: ContactPageComponent },
+  { path: 'login', component: SignInComponent },
+  {path:'register', component:SignUpComponent}
 ];
