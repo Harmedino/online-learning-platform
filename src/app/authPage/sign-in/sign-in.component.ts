469c8a348ee480bd08bc38bd23ff84d731451550
Harmedino <adamsahmed191@gmail.com>
2024-01-11 01:37:13 +0100

    store

--- src/app/authPage/sign-in/sign-in.component.ts
+++ src/app/authPage/sign-in/sign-in.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    FormsModule,RouterLink],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+
+  onLogin(form: NgForm) {
+
+  }
 
 }
--- src/app/authPage/sign-up/sign-up.component.ts
+++ src/app/authPage/sign-up/sign-up.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from '../../app.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule,
+    ReactiveFormsModule,
+  RouterLink],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
-
+  onSubmit(form: NgForm) {}
 }
